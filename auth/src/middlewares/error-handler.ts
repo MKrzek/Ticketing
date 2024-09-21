d9a549c938b006ff051faac328be8869d8cdbce5
mkrzek <mkrzek@googlemail.com>
2024-09-21 20:50:41 +0100

    feat: enforce error object shape by adding abstract class

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,5 @@
 import { Response, Request, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 
@@ -8,18 +9,10 @@ export const errorHandler = (
 	res: Response,
 	next: NextFunction
 ) => {
-	if (err instanceof RequestValidationError) {
-		const formattedErrors = err.errors.map((error) => {
-			if (error.type === 'field') {
-				return { message: error.msg, field: error.path };
-			}
-		});
-		return res.status(400).send({ errors: formattedErrors });
+	if (err instanceof CustomError) {
+		return res.status(err.statusCode).send({ errors: err.serializeErrors() });
 	}
 
-	if (err instanceof DatabaseConnectionError) {
-		return res.status(500).send({ errors: [{ message: err.reason }] });
-	}
 	res.status(400).send({
 		errors: [{ message: 'Something went wrong' }],
 	});
