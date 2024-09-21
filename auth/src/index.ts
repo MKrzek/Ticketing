74ac38fa5a8e4a32c881627793ff8fe021490aef
mkrzek <mkrzek@googlemail.com>
2024-09-21 21:11:48 +0100

    feat: page not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,18 +5,22 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
-app.use( json() );
+app.use(json());
 
-app.use( currentUserRouter )
-app.use( signinRouter )
-app.use( signupRouter )
-app.use( signoutRouter )
-app.use(errorHandler)
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
 
+app.all('*', () => {
+	throw new NotFoundError();
+});
 
+app.use(errorHandler);
 
-app.listen( 3000, () => {
-  console.log('Listening auth on 3000!!')
-})
+app.listen(3000, () => {
+	console.log('Listening auth on 3000!!');
+});
