0b157097f1edb72caf18276f4463fb294f1b51f8
mkrzek <mkrzek@googlemail.com>
2024-10-13 15:32:22 +0100

    feat: show if user is signed in

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,12 @@
 import buildClient from '../api/buildClient';
-const Landing = ( { currentUser } ) => {
-  console.log( 'in component', currentUser );
 
-  return <div>Helllo3333</div>;
+const Landing = ( { currentUser } ) => {
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 };
 
 export const getServerSideProps = async ( context ) => {
-
   const { data } = await buildClient( context ).get( '/api/users/currentuser' );
   return { props: { ...data } };
-
 };
 
 export default Landing;
