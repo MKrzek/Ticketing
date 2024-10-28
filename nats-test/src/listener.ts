949e7fce587652b9440c3de8b86dde5a07e7b58a
mkrzek <mkrzek@googlemail.com>
2024-10-28 20:34:29 +0000

    feat: nats streaming test project

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,23 @@
+import {randomBytes} from 'crypto';
+import nats, {Message} from 'node-nats-streaming';
+
+console.clear();
+// client
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+	url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+	console.log('Listener connected to nats');
+
+	const subscription = stan.subscribe(
+		'ticket:created',
+		'order-service-queue-group'
+	);
+	subscription.on('message', (msg: Message) => {
+		const data = msg.getData();
+		if (typeof data === 'string') {
+			console.log(`Received event #${msg.getSequence()}, with data: ${data}}`);
+		}
+	});
+});
