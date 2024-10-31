d5273fa34a6d8ee702ea6b6396db188eabc5a7d3
mkrzek <mkrzek@googlemail.com>
2024-10-31 21:45:22 +0000

    feat: add options to  listeners

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -3,21 +3,51 @@ import nats, {Message} from 'node-nats-streaming';
 
 console.clear();
 // client
+
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 	url: 'http://localhost:4222',
 });
 
+// send custom info back to nats streaming service that listener received messge
+// if we do not send an acknowledgement message back to nats streaming service, then nats streaming
+// after some time will try to send the message/event to another listener from the same queue group
+// run: k get pods
+// run: k port-forward pod-name 8222:8222
+// go to localhost:8222/streaming  - to see monitoring data
+
+// setDeliveredAllAv  option will make sure that the first time we run this listener, all events
+// that were unprocessed and were waiting in the channel to be processed, will be processed
+// setDurableName will make sure that the event that was processed, will not be processed again in a situation
+// when a listener is down and then back again
+const options = stan
+	.subscriptionOptions()
+	.setManualAckMode(true)
+	.setDeliverAllAvailable()
+	.setDurableName('order-service');
+
 stan.on('connect', () => {
 	console.log('Listener connected to nats');
 
+	stan.on('close', () => {
+		console.log('NATS connection closed');
+		process.exit();
+	});
+
 	const subscription = stan.subscribe(
 		'ticket:created',
-		'order-service-queue-group'
+		'order-service-queue-group',
+		options
 	);
 	subscription.on('message', (msg: Message) => {
 		const data = msg.getData();
 		if (typeof data === 'string') {
 			console.log(`Received event #${msg.getSequence()}, with data: ${data}}`);
 		}
+		msg.ack();
 	});
 });
+
+// interrupt and terminate request that are sent to this program are going to be intercepted
+// and we will try to close the  nats server connection
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
