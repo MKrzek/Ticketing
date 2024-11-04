0b08df23c51a3cc0d96531bd0569fc9b2aaf1a61
mkrzek <mkrzek@googlemail.com>
2024-11-04 20:30:00 +0000

    feat: nats-test refactor

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
 import {randomBytes} from 'crypto';
-import nats, {Message} from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
+import {TicketCreatedListener} from './events/ticket-created-listener';
 
 console.clear();
 // client
@@ -19,11 +20,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 // that were unprocessed and were waiting in the channel to be processed, will be processed
 // setDurableName will make sure that the event that was processed, will not be processed again in a situation
 // when a listener is down and then back again
-const options = stan
-	.subscriptionOptions()
-	.setManualAckMode(true)
-	.setDeliverAllAvailable()
-	.setDurableName('order-service');
+// const options = stan
+// 	.subscriptionOptions()
+// 	.setManualAckMode(true)
+// 	.setDeliverAllAvailable()
+// 	.setDurableName('order-service');
 
 stan.on('connect', () => {
 	console.log('Listener connected to nats');
@@ -33,18 +34,19 @@ stan.on('connect', () => {
 		process.exit();
 	});
 
-	const subscription = stan.subscribe(
-		'ticket:created',
-		'order-service-queue-group',
-		options
-	);
-	subscription.on('message', (msg: Message) => {
-		const data = msg.getData();
-		if (typeof data === 'string') {
-			console.log(`Received event #${msg.getSequence()}, with data: ${data}}`);
-		}
-		msg.ack();
-	});
+	new TicketCreatedListener(stan).listen();
+	// const subscription = stan.subscribe(
+	// 	'ticket:created',
+	// 	'order-service-queue-group',
+	// 	options
+	// );
+	// subscription.on('message', (msg: Message) => {
+	// 	const data = msg.getData();
+	// 	if (typeof data === 'string') {
+	// 		console.log(`Received event #${msg.getSequence()}, with data: ${data}}`);
+	// 	}
+	// 	msg.ack();
+	// });
 });
 
 // interrupt and terminate request that are sent to this program are going to be intercepted
