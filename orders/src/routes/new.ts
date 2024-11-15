02e78a716ee8fbb5298b04deac3af3f9bbfa27f0
mkrzek <mkrzek@googlemail.com>
2024-11-15 21:10:39 +0000

    feat: add  create an order route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,19 @@
-import {requireAuth, validateRequest} from '@mkrzektickets/common';
+import {
+	BadRequestError,
+	NotFoundError,
+	OrderStatus,
+	requireAuth,
+	validateRequest,
+} from '@mkrzektickets/common';
 import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
 import mongoose from 'mongoose';
+import {Order} from '../models/order';
+import {Ticket} from '../models/ticket';
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
 	'/api/orders',
@@ -17,7 +27,33 @@ router.post(
 	],
 	validateRequest,
 	async (req: Request, res: Response) => {
-		res.send({});
+		const {ticketId} = req.body;
+		// find the ticket the user is trying to order in DB
+		const ticket = await Ticket.findById(ticketId);
+		if (!ticket) {
+			throw new NotFoundError();
+		}
+		// make sure this ticket is not reserved
+
+		const isReserved = await ticket.isReserved();
+
+		if (isReserved) {
+			throw new BadRequestError('Ticket is already reserved');
+		}
+		// calculate expiration date (15mins) for this order
+		const expiration = new Date();
+		expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+		// build the order and save it to DB
+		const order = Order.build({
+			userId: req.currentUser!.id,
+			status: OrderStatus.Created,
+			expiresAt: expiration,
+			ticket,
+		});
+		await order.save();
+		// publish an event that an order was created
+		res.status(201).send(order);
 	}
 );
 
