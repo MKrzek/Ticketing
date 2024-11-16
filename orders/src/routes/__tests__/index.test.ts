18632aaa073fa58fc2070de7db5fdedbc39ce786
mkrzek <mkrzek@googlemail.com>
2024-11-16 17:38:08 +0000

    feat: add  order routes test

---
+++ orders/src/routes/__tests__/index.test.ts
@@ -0,0 +1,49 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+const buildTicket = async () => {
+	const ticket = Ticket.build({
+		title: 'concert',
+		price: 20,
+	});
+	await ticket.save();
+	return ticket;
+};
+
+it('fetches orders for a partcular user', async () => {
+	const ticketOne = await buildTicket();
+	const ticketTwo = await buildTicket();
+	const ticketThree = await buildTicket();
+
+	const user1 = global.signin();
+	const user2 = global.signin();
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', user1)
+		.send({ticketId: ticketOne.id})
+		.expect(201);
+
+	const {body: order1} = await request(app)
+		.post('/api/orders')
+		.set('Cookie', user2)
+		.send({ticketId: ticketTwo.id})
+		.expect(201);
+
+	const {body: order2} = await request(app)
+		.post('/api/orders')
+		.set('Cookie', user2)
+		.send({ticketId: ticketThree.id})
+		.expect(201);
+
+	const response = await request(app)
+		.get('/api/orders')
+		.set('Cookie', user2)
+		.expect(200);
+
+	expect(response.body.length).toEqual(2);
+	expect(response.body[0].id).toEqual(order1.id);
+	expect(response.body[1].id).toEqual(order2.id);
+	expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
+	expect(response.body[1].ticket.id).toEqual(ticketThree.id);
+});
--- orders/src/routes/__tests__/new.test.ts
+++ orders/src/routes/__tests__/new.test.ts
@@ -1,5 +1,51 @@
 import mongoose from 'mongoose';
+import request from 'supertest';
+import {OrderStatus} from '../../../../common/src/events/types/order-status';
+import {app} from '../../app';
+import {Order} from '../../models/order';
+import {Ticket} from '../../models/ticket';
 
 it('returns an error if the ticket does not exist', async () => {
 	const ticketId = new mongoose.Types.ObjectId();
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ticketId})
+		.expect(404);
 });
+
+it('returns an error if the ticket is reserved', async () => {
+	const ticket = Ticket.build({
+		title: 'concert',
+		price: 20,
+	});
+	await ticket.save();
+	const order = Order.build({
+		ticket,
+		userId: '12345667',
+		status: OrderStatus.Created,
+		expiresAt: new Date(),
+	});
+
+	await order.save();
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ticketId: ticket.id})
+		.expect(400);
+});
+
+it('reserve a ticket', async () => {
+	const ticket = Ticket.build({
+		title: 'concert',
+		price: 20,
+	});
+	await ticket.save();
+	await request(app)
+		.post('/api/orders')
+		.set('Cookie', global.signin())
+		.send({ticketId: ticket.id})
+		.expect(201);
+});
+
+it.todo('emit event after order was created');
