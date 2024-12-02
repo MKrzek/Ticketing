6deaa8a9a10fcc30ee4ce5eb5dbc53de9b1a67c2
mkrzek <mkrzek@googlemail.com>
2024-12-02 21:24:12 +0000

    feat: add order id to model

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import {updateIfCurrentPlugin} from 'mongoose-update-if-current';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
 	title: string;
@@ -12,6 +12,7 @@ interface TicketDoc extends mongoose.Document {
 	price: number;
 	userId: string;
 	version: number;
+	orderId?: string;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -31,6 +32,9 @@ const ticketSchema = new mongoose.Schema(
 		userId: {
 			type: String,
 			required: true,
+		},
+		orderId: {
+			type: String,
 		},
 	},
 	{
