70399903d711669ae310516234e927a97d478c7a
MKrzek <mkrzek@googlemail.com>
2025-02-17 21:13:59 +0000

    feat: add skaffolding for payment service

---
+++ payments/src/app.ts
@@ -0,0 +1,28 @@
+import 'express-async-errors';
+
+import { currentUser, errorHandler, NotFoundError } from '@mkrzektickets/common';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import express from 'express';
+
+
+const app = express();
+// traffic is proxied to express through ingress nginx and allows express to trust the connection even when express is behind the proxy
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== 'test',
+	})
+);
+app.use(currentUser);
+
+
+app.all('*', () => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler as any);
+
+export { app };
