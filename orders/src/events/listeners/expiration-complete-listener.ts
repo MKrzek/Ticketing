b07fe008fac2c7eed2ac4a31ec33f55c4c25f92d
MKrzek <mkrzek@googlemail.com>
2025-02-19 22:30:45 +0000

    fix: fix spelling

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -5,8 +5,9 @@ import { Order } from "../../models/order";
 import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
-    queueGroupName = queueGroupName
     readonly subject = Subjects.ExpirationComplete;
+    queueGroupName = queueGroupName
+
 
     async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
         const order = await Order.findById(data.orderId)
