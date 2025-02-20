27cb8e169ea12912710c013a0d6f517f04b3adb4
MKrzek <mkrzek@googlemail.com>
2025-02-20 23:00:17 +0000

    feat: wire up stripe

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -2,6 +2,7 @@ import { BadRequestError, NotAuthorizedError, NotFoundError, OrderStatus, requir
 import { body } from "express-validator";
 import express, { Request, Response } from 'express';
 import { Order } from "../models/order";
+import { stripe } from "../stripe";
 
 const router = express.Router()
 router.post('/api/payments',
@@ -21,9 +22,15 @@ router.post('/api/payments',
             throw new NotAuthorizedError()
         }
 
-        if (order.status = OrderStatus.Cancelled) {
+        if (order.status === OrderStatus.Cancelled) {
             throw new BadRequestError('Cannot pay for a cancelled order')
         }
+        await stripe.charges.create({
+            currency: 'usd',
+            amount: order.price * 100,
+            source: token
+        })
+
         res.send({ success: true })
     })
 
