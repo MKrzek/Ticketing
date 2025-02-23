1957122c06caf3496a56591fd428482fef9f39b3
MKrzek <mkrzek@googlemail.com>
2025-02-23 20:28:30 +0000

    feat: add listener for order cancelled in ticket service

--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -11,11 +11,14 @@ export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
 	async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
 		const ticket = await Ticket.findById(data.ticket.id);
 
+		console.log('IDDDDD', data.ticket.id)
+		console.log('SSS-found ticket', ticket)
+
 		if (!ticket) {
 			throw new Error('Ticket not found');
 		}
 
-		ticket.set({orderId: undefined});
+		ticket.set({ orderId: undefined });
 		await ticket.save();
 
 		await new TicketUpdatedPublisher(this.client).publish({
