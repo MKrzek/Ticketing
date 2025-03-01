05e3d45c48b28bfd763d4e5f59dbd80ed741c7e9
MKrzek <mkrzek@googlemail.com>
2025-03-01 22:04:56 +0000

    feat: pass prop to hook

--- client/hooks/useRequest.js
+++ client/hooks/useRequest.js
@@ -5,20 +5,20 @@ const useRequest = ( { url, method, body, onSuccess } ) => {
 
   const [errors, setErrors] = useState( null );
 
-  const doRequest = async () => {
+  const doRequest = async (props={}) => {
     try {
-      const response = await axios[method]( url, body );
+      const response = await axios[method](url, { ...body, ...props } );
       if ( onSuccess ) {
         onSuccess( response.data );
       }
       return response.data;
     } catch ( err ) {
-
+      console.log('ERRR', err)
       setErrors(
         <div className='alert alert-danger'>
           <h4>Ooops....</h4>
           <ul className='my-0'>
-            { err?.response?.data?.errors.map( error => <li key={ error.message }>{ error.message }</li> ) }
+            { err?.response?.data?.errors?.map(error => <li key={ error.message }>{ error.message }</li> ) }
           </ul>
         </div>
       );
