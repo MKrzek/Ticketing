02084344430707d7ab046efb23a3c3626fcb0734
MKrzek <mkrzek@googlemail.com>
2025-03-01 22:07:53 +0000

    feat: only fetch tickets that are not reserved

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -1,14 +1,14 @@
-import express, {Request, Response} from 'express';
-import {requireAuth, validateRequest} from '@mkrzektickets/common';
-import {body} from 'express-validator';
-import {Ticket} from '../models/ticket';
+import express, { Request, Response } from 'express';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
 router.get('/api/tickets', async (req: Request, res: Response) => {
-	const tickets = await Ticket.find({});
+	const tickets = await Ticket.find({
+		orderId: undefined
+	});
 
 	res.send(tickets);
 });
 
-export {router as indexTicketRouter};
+export { router as indexTicketRouter };
